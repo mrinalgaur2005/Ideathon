6bbcdfaba5288c8dad0dcf2fb4cf55a00e90f5b4
Vaibhav Verma <vaibhavverma7475@gmail.com>
2024-12-11 11:03:33 +0530

    events page done

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,13 +1,27 @@
 "use client"
 import EventCard from "@/components/events/card";
+import FilterBox from "@/components/events/filterbox";
 
 export default function EventsPage() {
 
 
   return (
     <>
-      <div className="flex flex-col w-full items-center">
-        <EventCard />
+      <div className="flex flex-row w-full ">
+        <div className="flex flex-col w-4/5 items-center">
+          <EventCard/>
+          <EventCard />
+          <EventCard />
+          <EventCard/>
+          <EventCard />
+          <EventCard />
+        </div>
+        <div className="flex flex-col w-1/5 items-center mt-8">
+          <button className="text-xl font-bold bg-gradient-to-br from-cyan-600 to-cyan-400 text-white w-36 rounded-3xl mb-8 mt-2">
+            Add Event
+          </button>
+          <FilterBox />
+        </div>
       </div>
     </>
   )
