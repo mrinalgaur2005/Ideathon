8c9af310069194be1c975f67ffb8e80acc03e773
“Prajanya-Sharma” <prajanyasharma.bt23ele@pec.edu.in>
2024-12-14 15:32:52 +0530

    Your descriptive commit message

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,18 @@
+import * as z from 'zod';
+
+export const signUpSchema = z.object({
+  username: z
+    .string()
+    .min(2, "Username must be at least 2 characters")
+    .max(20, "Username must be no more than 20 characters")
+    .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters"),
+  
+  email: z
+    .string()
+    .email("Invalid email format")
+    .regex(/^[a-zA-Z0-9._%+-]+@pec\.edu\.in$/, "Please use a valid college email ID"),
+  
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters"),
+});
