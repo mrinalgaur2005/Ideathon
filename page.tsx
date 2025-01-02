3ec87407a89063050472c566b79005c7abb33b73
Prajanya-Sharma <prajanyasharma.bt23ele@pec.edu.in>
2025-01-03 00:35:16 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import dynamic from 'next/dynamic'
+
+// Dynamically import OpenStreetMap component with SSR disabled
+const OpenStreetmap = dynamic(() => import('../../OpenStreetMap/ostm'), {
+  ssr: false,
+})
+
+const Index: React.FC = () => {
+  const [markers, setMarkers] = useState<{ username: string; latitude: number; longitude: number }[]>([])
+
+
+  return (
+    <>
+      <h1 className="text-center">OpenStreetMap</h1>
+      <OpenStreetmap />
+    </>
+  )
+}
+
+export default Index
