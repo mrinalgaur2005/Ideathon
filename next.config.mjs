ed99c60e5d07185997ea39e0e7002bccb5c11a96
mrinalgaur2005 <mrinalgaur30@gmail.com>
2025-01-14 22:46:16 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverExternalPackages: ['tesseract.js'],
-    outputFileTracingIncludes: {
-      '/api/**/*': ['./node_modules/**/*.wasm', './node_modules/**/*.proto']
-    }
-  };
-  
-  export default nextConfig;
-  
+  serverExternalPackages: ['tesseract.js'],
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/**/*.wasm', './node_modules/**/*.proto']
+  },
+  webpack: (config) => {
+    // Fix for `import type` issues
+    config.module.rules.push({
+      test: /\.(ts|js)$/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['next/babel']
+          }
+        }
+      ],
+      exclude: /node_modules/
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
